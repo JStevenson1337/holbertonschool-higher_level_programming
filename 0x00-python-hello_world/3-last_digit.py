a0d8ef6c0e6bbd01fe3147cf2f6a2924cb92572e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 16:18:38 -0600

    corrected conditional logic

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,10 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = (number % 10)
-if (last_digit > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-elif (last_digit == 0):
-    print( print("Last digit of {} is {} and is a zero".format(number, last_digit)))
+if (number == 0):
+    print("{} is zero".format(number % 10))
+    
+elif (number > 5):
+    print("Last digit of {} is {} and is greater than 5".format(number, number % 10))
+    
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    if(number > 0):
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
+    else:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % -10))
+
