62a1c1b4560d9f9f8ed62f9d8eb3958fb5a434dd
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 16:33:37 -0600

    checking 7 and 8

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Write a program that prints all possible different combinations of two digits.
+
+Numbers must be separated by ,, followed by a space
+The two digits must be different
+01 and 10 are considered the same combination of the two digits 0 and 1
+Print only the smallest combination of two digits
+Numbers should be printed in ascending order, with two digits
+The last number should be followed by a new line
+You can only use no more than 3 print functions with string format
+You can only use no more than 2 loops in your code
+You are not allowed to store numbers or strings in a variable
+You are not allowed to import any module
+'''
+
+while True:
+    
