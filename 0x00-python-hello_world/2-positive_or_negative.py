bc8a246b9a9d4793cdd24de87e847ad90ac81f7a
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 12:57:45 -0600

    removed tabs

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if (number == 0):
-	print("{} is zero".format(number))
+    print("{} is zero".format(number))
 elif (number > 0):
-	print("{} is positive".format(number))
+    print("{} is positive".format(number))
 else:
-	print("{} is negative".format(number))
+    print("{} is negative".format(number))
