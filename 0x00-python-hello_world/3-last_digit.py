c7aa77625c6bfbea6dd5438108d0e20b3a793d8f
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 16:05:21 -0600

    removed addidtional print

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,9 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = (abs(number) % 10)
-print(number,last_digit)
-
+last_digit = (number % 10)
 if (last_digit > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif (last_digit == 0):
