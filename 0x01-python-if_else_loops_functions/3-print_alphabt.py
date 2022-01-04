7528122d62e5aa934766629dc0d6b563dabffe62
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-04 13:32:11 -0600

    testing

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for c in range(97, 123):
     if (chr(c) == 101) | (chr(c) == 113):
         continue
     else:
-        print(chr(c), end = "");
+        print(chr(c), end = "")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if (i < 98):
-        print("{:02d}".format(i), end=', ');
+for i in range(0, 100):
+    if (i <= 98):
+        print("{:02d}".format(i), end=', ')
     else:
-        print(i, end='\n');
+        print("99", end='\n')
