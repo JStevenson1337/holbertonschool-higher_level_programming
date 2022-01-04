c9e2633c2e036d5136931dfd9ebaa8e440b6361e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-04 12:49:45 -0600

    corrected 2 and 5

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,3 @@
 
 for c in range(97, 123):
     print(chr(c), end = "")
-
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 i = 0
 
-
-while (i <= 98):
-    print("{:02d}".format(i), end=", ")
-    i = i + 1
-
-print("{}".format('99'), end='\n')
+while i <= 99:
+    if (i < 99):
+        print("{:02d}".format(i), end=', ')
+    else:
+        print(i, end='\n')
+    i += 1
