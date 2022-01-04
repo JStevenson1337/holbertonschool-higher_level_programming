e0f564fb90ee30e959f464523b3ee38456529f04
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 18:23:22 -0600

    corrected syntax

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,9 +2,9 @@
 i, j = 0, 0
 while i <= 9:
     while j <= 9:
-        if (i < j) & (i != j):
+        if (i < j - 1) & (i != j - 1):
             print("{}{}".format(i, j), end=', ')
-        if ((i == 8) & (j == 9)):
+        elif ((i == 8) & (j == 9)):
             print("{}{}".format(i, j), end='\n')
         j += 1
     i += 1
