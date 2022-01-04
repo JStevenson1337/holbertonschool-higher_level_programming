ec7d097bc6dfdf2f815aa936e614bcc8515cdf29
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-03 19:20:38 -0600

    corrected PEP8 Syntax

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -5,9 +5,12 @@ number = random.randint(-10000, 10000)
 if (number == 0):
     print("Last digit of {} is {} and is {}".format(number, number, number))
 elif (number > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, number % 10))
+    print("Last digit of {} is {} and \
+is greater than 5".format(number, number % 10))
 else:
     if(number > 0):
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
+        print("Last digit of {} is {} and \
+is less than 6 and not 0".format(number, number % 10))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % -10))
+        print("Last digit of {} is {} and is less \
+than 6 and not 0".format(number, number % -10))
