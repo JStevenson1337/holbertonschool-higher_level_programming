78c2be1a35c26ff4ec01feb9a0c541afad440749
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-04 17:53:46 -0600

    checking 8, 9, 10

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    i = 0
+    temp_str = ""
+    while (str[i:]):
+        ascii_i = ord(str[i])
+        if (ascii_i >= 97) & (ascii_i <= 122):
+            temp_str += chr(ascii_i - 32)
+        else:
+            temp_str += chr(ascii_i)
+        i += 1
+    print(temp_str)
