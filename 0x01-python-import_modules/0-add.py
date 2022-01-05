60dc7eaaf99a265b27ff28c5186f69d7ca9fc94c
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-05 14:56:06 -0600

    added include guard

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
-
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
