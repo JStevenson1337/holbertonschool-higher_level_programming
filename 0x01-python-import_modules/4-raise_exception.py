a63f9d8ce96f5f07e1f15537aba2a8ab9a1c1308
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-05 20:55:28 -0600

    checking 4

---
+++ 0x01-python-import_modules/4-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    my_string = "the string"
+    
+    new_string = my_string * 1.5
+    print(new_string)
--- 0x01-python-import_modules/4-raise_exception.py5-raise_exception_msg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-
-def raise_exception():
-    """Raise exception
-    """
-    raise TypeError("Exception raised")
