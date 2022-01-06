0c80366ddebe60f72069f2e312362934d50f642a
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-05 20:27:47 -0600

    corrected output

--- 0x01-python-import_modules/3-main.py
+++ 0x01-python-import_modules/3-main.py
@@ -10,3 +10,13 @@ a = 12
 b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 10
+b = -2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 0
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,14 +3,13 @@
 def safe_print_division(a, b):
     while True:
         try:
-            result = a / b
+            a / b
             break
         except ZeroDivisionError:
             break
         finally:
-            if b == 0:
-                print("Inside result: {}".format(None))
+            if b != 0:
+                print("Inside result: {}".format(a / b))
+                return (a / b)
             else:
-                print("Inside result: {}".format(a/b))
-            
-
+                print("Inside result: {}".format(None))
