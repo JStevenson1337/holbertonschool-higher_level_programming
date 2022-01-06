163a0aedebffd5f3a63f184ebe5ec5db6af8b875
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-06 13:38:42 -0600

    corr

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    i = 0
-    while i <= len(my_list):
-        print("{}".format(my_list[i]))
-        i =+ 1
+    for i in my_list:
+        print("{:d}".format(i))
