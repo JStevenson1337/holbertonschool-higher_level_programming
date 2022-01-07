56f99d78ebe89c457a190a1e04e5789e2699658e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-06 21:12:11 -0600

    test 4

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -4,9 +4,9 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return my_list
-    if element is None:
+    elif element is None:
         return my_list
     else:
         my_new_list = my_list.copy()
