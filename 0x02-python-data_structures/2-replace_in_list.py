3422980fd782c052d43114c0d9a4ae744daed8f3
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-06 20:29:10 -0600

    trying again

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@
 def replace_in_list(my_list, idx, element):
     my_list_copy = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1 or element is None:
-        return my_list
+        return my_list_copy
     my_list[idx] = element
-    return my_list_copy
+    return my_list
