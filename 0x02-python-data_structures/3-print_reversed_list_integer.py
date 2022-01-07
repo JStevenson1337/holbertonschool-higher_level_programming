c9cc8c44b5ab0d6b84203edcc4d466ceb6122a75
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-06 20:43:57 -0600

    added more formatting

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@
 def print_reversed_list_integer(my_list=[]):
     result = [t for t in reversed(my_list)]
     for i in result:
-        print('{:d}'.format(i))
+        print('{:d}'.format(i), end='\n' if i != result[-1] else '')
