f6de38c665bbc5a61254effc48a67b06c0670811
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-06 20:13:46 -0600

    still testing 2

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,6 @@
 
 
 def replace_in_list(my_list, idx, element):
-    print(len(my_list))
     if idx < 0 or idx > len(my_list) or element is None:
         return my_list
     my_list[idx] = element
