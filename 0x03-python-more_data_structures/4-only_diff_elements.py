4472cb8d843461261c64feba88394d0020abe90a
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 15:36:46 -0600

    Correcting syntax

--- 0x03-python-more_data_structures/4-only_diff_elements.py
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -5,6 +5,5 @@ def only_diff_elements(set_1, set_2):
     """
     Returns the elements that are in set_1 but not in set_2
     """
-    lambda_set_1 = set(map(lambda x: x, set_1))
-    lambda_set_2 = set(map(lambda x: x, set_2))
-    return lambda_set_1 | lambda_set_2
+    lambda_set_diff = set(map(lambda x: x, set_1)) | set(map(lambda x: x, set_2))
+    return lambda_set_diff - (set_1 & set_2)
