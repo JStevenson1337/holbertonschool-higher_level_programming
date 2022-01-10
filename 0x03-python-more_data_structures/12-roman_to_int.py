9dd965b7d6aef8d5464997ec6187b73376b99b37
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 15:50:31 -0600

    checking 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if roman_string \
+    is not isinstance(roman_string, str) \
+        | len(roman_string) == 0:
+        return 0
+    
+    while (True):
+        if roman_string == "":
+            return 0
+        elif roman_string[0] == "I":
+            return 1 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "V":
+            return 5 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "X":
+            return 10 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "L":
+            return 50 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "C":
+            return 100 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "D":
+            return 500 + roman_to_int(roman_string[1:])
+        elif roman_string[0] == "M":
+            return 1000 + roman_to_int(roman_string[1:])
+        else:
+            return 0
