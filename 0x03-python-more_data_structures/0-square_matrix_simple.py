d73d7b8d168e5f06e2eebddb209414debc686ecb
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 13:10:41 -0600

    check 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-import py_compile
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 
 
 def square_matrix_simple(matrix=[]):
-    [[row[i] for row in matrix] for i in range(3)]
-
-
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
-
-filename = '0-square_matrix_simple.py'
-compile()
+    """
+    Return a new list with the square of each element of the matrix.
+    """
+    new_list = [[element**2 for element in row] for row in matrix]
+    return new_list
