12e294e2ae0fcb657e29e8fa14125fb84003d5c0
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 15:14:14 -0600

    corrected syntax

--- 0x03-python-more_data_structures/5-number_keys.py
+++ 0x03-python-more_data_structures/5-number_keys.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
 
-
 def number_keys(a_dictionary):
     """Return the number of keys in a dictionary.
     """
-
     return dict_len(a_dictionary)
 
 
 def dict_len(a_dictionary):
     """Return the number of keys in a dictionary.
     """
-
     return len(a_dictionary)
