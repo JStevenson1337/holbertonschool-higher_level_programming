44364aa5a32eaf0a15780640294b51e063cb1357
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 16:21:39 -0600

    Correcting syntax

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string == "":
-        return None
+    if roman_string == None:
+        return 0
     elif type(roman_string) != str:
-        return None
+        return 0
     else:
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                       'M': 1000}
