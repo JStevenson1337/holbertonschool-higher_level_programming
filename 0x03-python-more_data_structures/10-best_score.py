96f944733e2bb40c073a76cecee81c9fe502c7e7
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 19:05:09 -0600

    add additional NULL check

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -6,6 +6,8 @@ def best_score(a_dictionary):
     '''
     if a_dictionary is None:
         return None
+    elif len(a_dictionary) == 0:
+        return None
     else:
         best = lambda x, y: x if x > y else y
         return max(a_dictionary, key=lambda x: a_dictionary[x])
