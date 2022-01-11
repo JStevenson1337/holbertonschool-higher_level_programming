973528621aa3b79289c1469dc30610ea513cb8ca
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 19:10:17 -0600

    syntax

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -11,13 +11,3 @@ def best_score(a_dictionary):
     else:
         return max(a_dictionary, key=lambda x: a_dictionary[x])
 
-
-def best_lamda(a_dictionary):
-    ''' Returns the key with the highest value in a dictionary
-    '''
-    if a_dictionary is None:
-        return None
-    elif len(a_dictionary) == 0:
-        return None
-    else:
-        return max(a_dictionary, key=lambda x: a_dictionary[x])
--- 0x03-python-more_data_structures/5-number_keys.py
+++ 0x03-python-more_data_structures/5-number_keys.py
@@ -5,9 +5,3 @@ def number_keys(a_dictionary):
     """Return the number of keys in a dictionary.
     """
     return dict_len(a_dictionary)
-
-
-def dict_len(a_dictionary):
-    """Return the number of keys in a dictionary.
-    """
-    return len(a_dictionary)
