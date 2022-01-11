df413353fddf4d007e8bd6a6a202b3d401328296
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 19:10:58 -0600

    undo changes

--- 0x03-python-more_data_structures/5-number_keys.py
+++ 0x03-python-more_data_structures/5-number_keys.py
@@ -5,3 +5,9 @@ def number_keys(a_dictionary):
     """Return the number of keys in a dictionary.
     """
     return dict_len(a_dictionary)
+
+
+def dict_len(a_dictionary):
+    """Return the number of keys in a dictionary.
+    """
+    return len(a_dictionary)
