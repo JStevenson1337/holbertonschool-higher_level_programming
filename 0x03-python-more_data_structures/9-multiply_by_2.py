30b145216725a702c51d6857711126678cb5c61e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 18:56:14 -0600

    checking 9

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -2,3 +2,9 @@
 
 
 def multiply_by_2(a_dictionary):
+    '''Multiplies each value in a dictionary by 2.
+    '''
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
