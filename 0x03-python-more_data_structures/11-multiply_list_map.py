e983f336f647b419096594b1dc26f8572a398f24
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-10 19:18:06 -0600

    removed comment

--- 0x03-python-more_data_structures/11-multiply_list_map.py
+++ 0x03-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], number=0):
-    ''' Multiplies all items in a list
-    '''
-    new_list = []
     return(list(map(lambda x: x * number, my_list)))
