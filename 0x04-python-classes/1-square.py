191571ec152e93942a30a4ba942e24b1760c58d4
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-11 20:03:34 -0600

    corrected 1 and 2

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -2,16 +2,13 @@
 "A simple Square class that defines a square by its size."
 
 
-TypeError = "size must be an integer"
-ValueError = "size must be >= 0"
-
 class Square:
     "A simple Square class that defines a square by its size."
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -2,16 +2,13 @@
 "A simple Square class that defines a square by its size."
 
 
-TypeError = "size must be an integer"
-ValueError = "size must be >= 0"
-
 class Square:
     "A simple Square class that defines a square by its size."
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
@@ -25,7 +22,7 @@ class Square:
     @size.setter
     def size(self, value):
         if type(value) != int:
-            raise TypeError
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError
+            raise ValueError("size must be >= 0")
         self.__size = value
