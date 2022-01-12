43434578242426b457b3ce8e71a52e7f2d6f786c
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-11 18:25:45 -0600

    added doc

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
+"A simple Square class that defines a square by its size."
 
 
 class Square:
     """
     Square class
     """
+    
     def __init__(self, size):
         """
         Initialize square
         """
         self.__size = size
+        
