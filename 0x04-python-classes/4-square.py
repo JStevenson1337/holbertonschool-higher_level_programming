144069d82e244a2d7cfdbe81862a5b3f43b0b412
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-11 21:37:34 -0600

    docstring update

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -7,13 +7,22 @@ class Square:
 
     Attributes:
         __size (int): The size of the square.
+
         Return:
             int : The area of the square.
+
         Raise:
             TypeError: size must be an integer
             ValueError: size must be >= 0
     """
     def __init__(self, size=0):
+        """Initializes the square.
+        `size` must be an integer and >= 0.
+        `pep8` recommends to use `self.__size` instead of `self.size`.
+        
+        Returns:
+            None
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -43,6 +52,7 @@ class Square:
 
         Returns:
             int : The Value of the size.
+
             Raises:
                 TypeError: If size is not an integer.
                 ValueError: If size is less than 0.
@@ -75,6 +85,7 @@ class Square:
         """Prints to stdout the square with the character.
                 Prints # if size is 0.
                 Prints # * size times.
+
             Returns:
                 None
         """
