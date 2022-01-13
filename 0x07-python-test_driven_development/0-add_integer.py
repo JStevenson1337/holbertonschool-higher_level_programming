88fdc4cb08ee9fa5b2da1fd8d5e50cdd210a6d1a
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-13 15:08:13 -0600

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Add two integers """
+
+def add_integer(a, b=98):
+    """ Add two integers
+
+    Args:
+        a (int): [Mandatory int to pass in]
+        b (int): [Optional int to pass in]
+    Returns:
+        int: [Sum of a and b]
+    """
+    if type(a) is int or type(b) is int:
+        pass
+    elif type(a) is float:
+        a = int(a)
+    elif type(b) is float:
+        b = int(b)
+    else:
+        if type(a) is not int:
+            raise TypeError("a must be an integer")
+        if type(b) is not int:
+            raise TypeError("b must be an integer")
+    return a + b
+
+
+
+
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
