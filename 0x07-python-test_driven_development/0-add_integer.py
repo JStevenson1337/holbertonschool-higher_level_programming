b4a1cbace9ef89d3aa10dfe5b7912106dcc8fa24
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-13 16:55:44 -0600

    adding 0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Add two integers """
 
+
 def add_integer(a, b=98):
     """ Add two integers
 
@@ -10,32 +11,18 @@ def add_integer(a, b=98):
     Returns:
         int: [Sum of a and b]
     """
-    if type(a) is int or type(b) is int:
+    if type(a) is int:
         pass
     elif type(a) is float:
         a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+
+    if type(b) is int:
+        pass
     elif type(b) is float:
         b = int(b)
     else:
-        if type(a) is not int:
-            raise TypeError("a must be an integer")
-        if type(b) is not int:
-            raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
+
     return a + b
-
-
-
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
