5559f31104759cddb096e88c0fd2d8134dc39040
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-14 15:42:40 -0600

    saving progress

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,43 +10,27 @@ def matrix_divided(matrix, div):
     Returns:
         matrix:
     """
-    if not check_len(matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-
-def looping_the_matrix(matrix):
-    """ Function to iterate over the matrices
-
-    Args:
-        matrix ([[list]]): matrixt to loop over
-    """
-    for i in matrix:
-            for j in i:
-                print(j, type(j))
-            print(i, type(i))
-    return (matrix[i][j])
-
-def check_len(matrix):
-    """[summary]
-
-    Args:
-        matrix ([type]): [description]
-    """
-    looper = iter(matrix)
-    measure = len(next(looper))
-    if not all(len(l) == measure for l in looper):
-        return False
-    else:
-        return True
-
-
+    # with matrix as m:
+    #     try:
+    #         pass
+        
+            
 
 matrix = [
-    [1, 2, 3, 5],
+    [1, 2, 3],
     [4, 5, 6]
 ]
+print(type(matrix),matrix)
+for a_list in matrix:
+    print(type(a_list), a_list)
+print(type())
 print(matrix_divided(matrix, 3))
 print(matrix)
 
 
-# if len({len(i) for i in lists}) == 1:   #list comprehension to preserver length
+# matrix = [
+#     [.01, 222, 793],
+#     [3, 9, 0]
+# ]
+# print(matrix_divided(matrix, 3))
+# print(matrix)
