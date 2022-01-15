f4b3207ce3ceda35da02d0a9c5b9ddf62fe3cb2b
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-15 12:49:43 -0600

    almost

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Divides all elements of a matrix"""
 
-
 def matrix_divided(matrix, div):
     """ Divides all elements of a matrix
     Args:
@@ -10,27 +9,130 @@ def matrix_divided(matrix, div):
     Returns:
         matrix:
     """
-    # with matrix as m:
-    #     try:
-    #         pass
+    try:
+        identify(matrix)
+        print(identify(matrix))
+        measure(matrix)
+        print(measure(matrix))
+        mutate(matrix, 3)
+        print(mutate(matrix, div),len(matrix))
+    except:
+        pass
+
+def identify(matrix):
+    """[sumary]
+    Args:
+        matrix ([type]): [description]
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not measure(matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not all(type(i) in (int, float) for i in matrix[0]):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    return True
+
+def mutate(matrix, div):
+    """ Divide element in matrix by div """
+    and_done = []
+    while True:
+        try:
+            args = list(len(matrix))
+            for i in matrix:
+                for j in i:
+                    and_done.append(j / div)
+            and_done = list(len(len(matrix)))
         
-            
+
+        except ZeroDivisionError:
+            return None
+        except TypeError:
+            return None
+        except Exception:
+            return None
+        else:
+            return and_done
+
+def measure(matrix):
+    """[summary]
+    Args:
+        matrix ([type]): [description]
+    """
+    looper = iter(matrix)
+    measure = len(next(looper))
+    if not all(len(l) == measure for l in looper):
+        return False
+    else:
+        return True
+
+
+
+
 
 matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
-print(type(matrix),matrix)
-for a_list in matrix:
-    print(type(a_list), a_list)
-print(type())
+
 print(matrix_divided(matrix, 3))
 print(matrix)
 
+def identify(matrix):
+    """[sumary]
+    Args:
+        matrix ([type]): [description]
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not measure(matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not all(type(i) in (int, float) for i in matrix[0]):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    return True
 
-# matrix = [
-#     [.01, 222, 793],
-#     [3, 9, 0]
-# ]
-# print(matrix_divided(matrix, 3))
-# print(matrix)
+
+
+def mutate(matrix, div):
+    """ Divide element in matrix by div """
+    and_done = []
+    while True:
+        try:
+            for i in matrix:
+                for j in i:
+                    and_done.append(j / div)
+        except ZeroDivisionError:
+            return None
+        except TypeError:
+            return None
+        except Exception:
+            return None
+        else:
+            return and_done
+
+
+
+
+
+def measure(matrix):
+    """[summary]
+    Args:
+        matrix ([type]): [description]
+    """
+    looper = iter(matrix)
+    measure = len(next(looper))
+    if not all(len(l) == measure for l in looper):
+        return False
+    else:
+        return True
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+print(matrix_divided(matrix, 3))
+print(matrix)
