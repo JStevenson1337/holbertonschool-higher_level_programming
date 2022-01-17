12ce086dd648a96a0fee873b6ba6453c0145692d
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-17 16:48:42 -0600

    doc string

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -3,23 +3,19 @@
 
 
 class Rectangle:
-    def __init__(self, width, height):
-        """
+    """
         Initializes the rectangle.
         `width` and `height` must be integers and >= 0.
         `pep8` recommends to use `self.__width` instead of `self.width`.
         Returns:
             None
-        """
+    """
+    def __init__(self, width, height):
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Getter for width.
-        Returns: int: The width of the rectangle.
-        Raises: TypeError: If width is not an integer.
-        """
         if self.width is not (type(int)):
             raise TypeError("width must be an integer")
         elif self.width < 0:
@@ -29,10 +25,6 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter for height.
-        Returns: int: The height of the rectangle.
-        Raises: TypeError: If height is not an integer.
-        """
         if self.height is not (type(int)):
             raise TypeError("height must be an integer")
         elif self.height < 0:
