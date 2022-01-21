f60da14ebed18767c52e6735ebe545f29bfe4028
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 14:40:07 -0600

    Trying something different

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" save_to_json_file.py: save to json file """
+""" Write a file """
 
 
 def write_file(filename="", text=""):
     """
-    Write a string to a text file (UTF8) and return the number of characters
+        Write a string to a text file (UTF8)
+        and return the number of characters
     """
     try:
-        with open(filename, encoding='utf-8') as f:
-            if type(text) is list:
-                for i in text:
-                    f.write(i)
-            else:
-                print(f.write(text), end="")
-        return len(text)
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(text)
     except IOError:
-        return 0
+        pass
     finally:
+        return len(text)
         f.close()
