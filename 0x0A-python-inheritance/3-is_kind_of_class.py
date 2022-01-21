3c67fdc95a7f7f31569a8c8663f169606b497e1a
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-20 19:31:09 -0600

    corrected pep8 syntax

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Python Inheritance """
 
+
 def is_kind_of_class(obj, a_class):
     """
-    Return True if the object is an instance of, or if the object is an instance
-    of a class that inherited from, the specified class; otherwise False.
+    Return True if the object is an instance of,
+    or if the object is an instance
+
     """
     return isinstance(obj, a_class)
