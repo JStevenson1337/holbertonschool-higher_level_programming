8b60c679b5a1f6c195d24d66cb7a028b97eb57bd
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-20 23:40:13 -0600

    checking 10 and 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """
     Class Square
     """
