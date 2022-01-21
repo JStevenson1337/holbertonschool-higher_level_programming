ae59a9b98fcb8d309fe0f55798bdb6cd9a10cddc
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-20 19:00:38 -0600

    updated method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """ My list class """
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+
     def print_sorted(self):
         """ Print the list in ascending order """
         print(sorted(self))
