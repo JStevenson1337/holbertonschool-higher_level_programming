d805c7e65832403f395b5ed396ae1bd73cec26e6
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 21:31:08 -0600

    checking 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Student Class """
+
+
+class Student:
+    """ A Student Class """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the Student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean) for JSON
+        """
+        if type(attrs) is not list:
+            return self.__dict__
+        for i in attrs:
+            if type(i) is not str:
+                return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            self.__dict__[key] = value
