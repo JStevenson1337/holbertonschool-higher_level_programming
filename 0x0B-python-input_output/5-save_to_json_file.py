02a2e52adabfcb32b07d4a488c98c7413a1c1838
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 21:53:10 -0600

    corrected pep8 syntax

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ def save_to_json_file(my_obj, filename):
     """
     Write an object to a text file using JSON representation
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,6 @@
     adds all arguments to a Python list,
     and then save them to a file in JSON format
 """
-
-
 import sys
 save_json = __import__("5-save_to_json_file").save_to_json_file
 load_json = __import__("6-load_from_json_file").load_from_json_file
