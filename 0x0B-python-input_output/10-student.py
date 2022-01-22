e6f6a90b6d7fdb7a47c85ec56e8239b5edd039ea
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 20:23:14 -0600

    Checking 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,3 +2,13 @@
 """ Student Class """
 
 Student = __import__('9-student').Student
+
+
+def to_json(self, attrs=None):
+    """ returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON
+    """
+    if attrs is None:
+        return self.__dict__
+    else:
+        return {key: value for key, value in self.__dict__.items() if key in attrs}
