918f47f36254d70f5e95e3cd193da327f933c773
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 20:33:56 -0600

    modified list constructor

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """ Student Class """
 
-Student = __import__('9-student').Student
 
+class Student:
+    """ A Student Class """
 
-def to_json(self, attrs=None):
-    """ returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON
-    """
-    if type(attrs) is list:
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the Student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean) for JSON
+        """
+        if type(attrs) is not list:
+            return self.__dict__
         for i in attrs:
             if type(i) is not str:
                 return self.__dict__
         return {k: v for k, v in self.__dict__.items() if k in attrs}
-    else:
-        return self.__dict__
