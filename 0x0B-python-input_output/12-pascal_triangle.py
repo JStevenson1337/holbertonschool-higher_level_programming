40f37acd10f9318cdc82c4329381b6a508277188
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-01-21 21:38:12 -0600

    Checking 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    """
+    Prints Pascal's Triangle
+    """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
