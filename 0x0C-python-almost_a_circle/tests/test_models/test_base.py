00d09379d4af420e9cd166ed473d3b7bea234164
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-01-28 18:17:04 -0600

    changed tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Unittesting for the Base module/class
-Tests are done for each method of the class"""
-import pep8
-import unittest
+"""
+Test for Base Class
+"""
+import inspect
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+import unittest
+import json
 
 
-class TestClassBase(unittest.TestCase):
-    """Test class for testing Base class"""
+class TestBaseClass(unittest.TestCase):
+    """
+    Test Base Class
+    """
 
     def test_pep8_base(self):
-            """
-            Test that models/base.py is pep8 compliant.
-            """
-            pep8style = pep8.StyleGuide(quiet=True)
-            result = pep8style.check_files(['models/base.py'])
-            self.assertEqual(result.total_errors, 0,
-                            "Found code style errors (and warnings).")
+        """
+        Test that models/base.py is pep8 compliant.
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_pep8_test_base(self):
         """
@@ -42,7 +44,7 @@ class TestClassBase(unittest.TestCase):
 
     def test_func_docstrings(self):
         """
-        Tests docstrings functions
+        Tests for the presence of docstrings in all functions
         """
         self.assertTrue(len(Base.__init__.__doc__) >= 1)
         self.assertTrue(len(Base.to_json_string.__doc__) >= 1)
@@ -51,18 +53,66 @@ class TestClassBase(unittest.TestCase):
         self.assertTrue(len(Base.create.__doc__) >= 1)
         self.assertTrue(len(Base.load_from_file.__doc__) >= 1)
 
+    def test_to_json_string(self):
+        """Tests regular to json string"""
+        Base._Base__nb_objects = 0
+        d1 = {"id": 98, "width": 5, "height": 6, "x": 7, "y": 8}
+        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
+        json_s = Base.to_json_string([d1, d2])
+        self.assertTrue(type(json_s) is str)
+        d = json.loads(json_s)
+        self.assertEqual(d, [d1, d2])
+
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_fjs_empty(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(""))
+
+    def test_fjs_None(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(None))
+
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_from_json_string(self):
+        """Tests regular from_json_string"""
+        json_str = '[{"id": 98, "width": 5, "height": 6, "x": 7, "y": 8}, \
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertEqual(json_l[0],
+                         {"id": 98, "width": 5, "height": 6, "x": 7, "y": 8})
+        self.assertEqual(json_l[1],
+                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+
+
 class NewTest(unittest.TestCase):
-    def test_id_sets(self):
-        """ 
+    def test_no_id_set(self):
+        """
         Tests no passed ID
         Passed id
         then no passed ID again
         """
         b1 = Base()
-        b98 = Base(98)
-        b2 = Base()
         self.assertEqual(b1.id, 1)
+
+    def test_id_set(self):
+        b98 = Base(98)
         self.assertEqual(b98.id, 98)
+
+    def test_no_id_set_2(self):
+        b2 = Base()
         self.assertEqual(b2.id, 2)
 
     def test_too_many_args(self):
@@ -71,3 +121,19 @@ class NewTest(unittest.TestCase):
         """
         with self.assertRaises(TypeError):
             b = Base(1, 1)
+
+    def test_nb_public(self):
+        """Tests nb_objects as a private instance attribute"""
+        b = Base(3)
+        with self.assertRaises(AttributeError):
+            print(b.nb_objects)
+
+    def test_nb_private(self):
+        """Tests nb_objects as a private instance attribute"""
+        b = Base(4)
+        with self.assertRaises(AttributeError):
+            print(b.__nb_objects)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Unittesting for the Square module/class
 Tests are done for each method of the class"""
-import pep8
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
