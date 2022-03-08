32707cb737a45644797305d5684b2687dc68c0ef
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-03-08 15:08:02 -0600

    modified function

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,17 +2,14 @@
 """ query the status of a given url """
 
 
-def hbtn_status():
+if __name__ == "__main__":
+    import urllib.request as rq
+
     """ Function that reads the content of a URL """
-    from urllib.request import urlopen, Request
-    req = Request("http://intranet.hbtn.io/status")
-    with urlopen(req) as response:
+    req = rq.Request("https://intranet.hbtn.io/status")
+    with rq.urlopen(req) as response:
         site = response.read()
         print("Body response:")
         print(f"\t- type: {type(site)}")
         print(f"\t- content: {site}")
         print(f"\t- utf8 content: {site.decode('utf-8')}")
-
-
-if __name__ == "__main__":
-    hbtn_status()
