3b4088bb0d6a717570653b20e3c4cd2fe3f8dfec
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-04 14:23:04 -0500

    fix: 0-2 add: 3 and 4

--- 0x00-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x00-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-// 1-multi_languages.js
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// 1-multi_languages.js
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
