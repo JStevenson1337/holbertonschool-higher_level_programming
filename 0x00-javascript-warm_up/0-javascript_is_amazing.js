e12ef72609bac990a1329c21c1b6b15e69e55406
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-04 14:06:35 -0500

    add: 2 and 3

--- 0x00-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x00-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // 1-multi_languages.js
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // 1-multi_languages.js
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
