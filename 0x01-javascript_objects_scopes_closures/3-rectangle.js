81440fc80cca97bca466cb3a870ca80c923a7cb3
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-05 17:41:02 -0500

    fix:

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (_w, _h) {
-    if ( _w > 0 && _h > 0) {
+    if ( _w > 0 && _h > 0 ) {
       this.width = _w;
       this.height = _h;
     }
   }
   print () {
-    newFunction();
-
-    function newFunction() {
-      for (let i = 0; i < this.height; i++) {
-        console.log("X".repeat(this.width));
-      }
+    for (let i = 0; i < this.height; i++) {
+      console.log("X".repeat(this.width));
     }
   }
 };
