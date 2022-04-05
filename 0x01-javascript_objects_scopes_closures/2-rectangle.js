6e5217eefe5784ccc29e4d8b7f3336e5d89eff05
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-05 17:38:17 -0500

    fix: 2-3

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (_w, _h) {
-    if ( (_w > 0 ) && ( _h > 0) ) {
+    if (_w > 0 && _h > 0) {
       this.width = _w;
       this.height = _h;
     }
--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (_w, _h) {
-    if (( _w > 0 ) && ( _h > 0)) {
+    if ( _w > 0 && _h > 0) {
       this.width = _w;
       this.height = _h;
     }
   }
   print () {
-    for (let i = 0; i < this.height - 2; i++) {
-      console.log("X".repeat(this.width));
+    newFunction();
+
+    function newFunction() {
+      for (let i = 0; i < this.height; i++) {
+        console.log("X".repeat(this.width));
+      }
     }
   }
 };
