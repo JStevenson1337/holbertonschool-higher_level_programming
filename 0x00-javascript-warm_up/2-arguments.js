7e42c587aba0851d02d59eade31cd3776cb2a84d
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-06 10:18:17 -0500

    Fix:

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -2,6 +2,7 @@
 if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
-} else{console.log('Arguments found');
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
