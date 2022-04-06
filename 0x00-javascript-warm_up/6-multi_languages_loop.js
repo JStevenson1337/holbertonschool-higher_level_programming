246165ad18c84e679f24e88981b7642f1c0dd74e
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-04-06 10:47:20 -0500

    Fix:

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < line.length; i++) {
-console.log(line[i]);
+  console.log(line[i]);
 }
